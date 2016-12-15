dcfae14161a843fcc7b4a282c2fa35e4f2524959
javanux <javanuxenergy@gmail.com>
2016-12-15 14:45:59 +0545

    first commit

---
+++ init0/ec2meta.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REGION="us-west-1"
+
+## Getting the ec2 metadata
+echo "fetching ec2 metadata from $REGION"
+until /usr/bin/ec2metadata | tee /tmp/meta; do :; done
+
+## Getting the instance_id
+echo "fetching instance id from /tmp/meta"
+instance_id=$(awk -F': ' "/instance-id/"'{print $2}' /tmp/meta)
+echo "instance-id: $instance_id"
+
+### Getting the tags
+echo "getting tags for $instance_id from $REGION"
+until /usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --region="$REGION" --output=text | tee /tmp/tags; do :; done
+
+### Getting the tags:shutdown_today
+echo "AWKing the tags from /tmp/tags"
+shutdown_today=$(/usr/bin/awk "/shutdown_today/"'{print $5}' < /tmp/tags)
+echo "tags:shutdown_today=$shutdown_today"
+
+
+### checking the shutdown_today value
+echo "checking the shutdown_today value"
+if [ "$shutdown_today" = "False" ]
+then
+        echo "Do not shutdown"
+        exit 1
+else
+        echo "Shutdown the system"
+        exit 0
+fi
