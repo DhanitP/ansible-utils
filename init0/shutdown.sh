5ebf4c5caa7c560a3e2f64a33f3b6794ceee03b5
javanux <javanuxenergy@gmail.com>
2016-12-16 15:09:25 +0545

    Doing some UI/UX for log

--- init0/shutdown.sh
+++ init0/shutdown.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-echo
-echo "See the log in $LOG_PATH"
+
 REGION="us-west-1"
+META=/tmp/meta
+TAGS=/tmp/tags
 
 ## Getting the ec2 metadata
-echo "fetching ec2 metadata from $REGION"
-until /usr/bin/ec2metadata | tee /tmp/meta; do :; done
+echo "fetching ec2 metadata from $REGION..."
+until /usr/bin/ec2metadata | tee $META; do :; done
 
 ## Getting the instance_id
-echo "fetching instance id from /tmp/meta"
-instance_id=$(awk -F': ' "/instance-id/"'{print $2}' /tmp/meta)
+echo
+echo
+echo "fetching instance id from $META..."
+instance_id=$(awk -F': ' "/instance-id/"'{print $2}' $META)
 echo "instance-id: $instance_id"
 
 ### Getting the tags
-echo "getting tags for $instance_id from $REGION"
-until /usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --region="$REGION" --output=text | tee /tmp/tags; do :; done
+echo
+echo
+echo "getting tags for $instance_id from $REGION..."
+until /usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" --region="$REGION" --output=text | tee $TAGS; do :; done
 
-### Getting the tags:shutdown_today
-echo "AWKing the tags from /tmp/tags"
-shutdown_today=$(/usr/bin/awk "/shutdown_today/"'{print $5}' < /tmp/tags)
-echo "tags:shutdown_today=$shutdown_today"
+### AWKing the tags:shutdown_today
+echo
+echo
+echo "AWKing the tags from $TAGS..."
+shutdown_today=$(/usr/bin/awk "/shutdown_today/"'{print $5}' < $TAGS)
+echo "Found tags:shutdown_today=$shutdown_today"
 
-
-### checking the shutdown_today value
-echo "checking the shutdown_today value"
+### Do or Die 
+echo
+echo
+echo "DO or Die ..."
 if [ "$shutdown_today" = "False" ]
 then
     echo "Do not shutdown"
