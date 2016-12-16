e1c8f34383f26656b628442e11c5037b83ddb545
javanux <javanuxenergy@gmail.com>
2016-12-16 14:54:20 +0545

    making a single script file

--- init0/cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-
-echo
-echo "See the log in $LOG_PATH"
-source shutdown.sh
--- init0/shutdown.sh
+++ init0/shutdown.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+echo
+echo "See the log in $LOG_PATH"
 REGION="us-west-1"
 
 ## Getting the ec2 metadata
