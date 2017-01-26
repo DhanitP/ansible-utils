da340635c01deddee920775ddab2ed68a984586c
Balman Rawat <javanuxenergy@gmail.com>
2017-01-26 15:17:41 +0545

    Update shutdown.sh

--- init0/shutdown.sh
+++ init0/shutdown.sh
@@ -12,7 +12,7 @@ function time_to_int(){
 
     hours=$(echo $1 | awk -F':'  '{print $1}')
     minutes=$(echo $1 | awk -F':'  '{print $2}')
-    minutes=$(echo "$hours*60 + $minutes" | bc)
+    minutes=$(( $hours*60 + $minutes ))
     echo $minutes
 }
 
